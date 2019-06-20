24fc2c4352fff3d310096faf7c6c3b81773a5ffb
freeintelligence <free04381@gmail.com>
2019-06-20 19:14:43 -0400

    Ahora las validaciones no interrumpen al validador 'required'.

--- projects/ngx-number-validation/src/lib/validators/max.validator.ts
+++ projects/ngx-number-validation/src/lib/validators/max.validator.ts
@@ -7,6 +7,10 @@ import { Utils } from 'number-validation';
  */
 export function MaxValidator(max: string | number, decimalSeparator: string): ValidatorFn {
   return (control: AbstractControl): { [key: string]: any } | null => {
+    if ((typeof control.value === 'string' && !control.value.length) || (control.value === null) || (isNaN(control.value) || typeof control.value === 'undefined')) {
+      return null;
+    }
+
     return Utils.toInt(control.value, decimalSeparator) > Utils.toInt(max, decimalSeparator) ? { numberMax: { value: control.value } } : null;
   };
 }
