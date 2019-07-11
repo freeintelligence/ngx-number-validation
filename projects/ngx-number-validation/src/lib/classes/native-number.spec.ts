5c5fd9c53a577c4ea99f78547f32641e3bb2e51d
freeintelligence <free04381@gmail.com>
2019-07-11 07:31:25 -0400

    Detalle..

--- projects/ngx-number-validation/src/lib/classes/native-number.spec.ts
+++
@@ -1,7 +0,0 @@
-import { NativeNumber } from './native-number';
-
-describe('NativeNumber', () => {
-  it('should create an instance', () => {
-    expect(new NativeNumber()).toBeTruthy();
-  });
-});
